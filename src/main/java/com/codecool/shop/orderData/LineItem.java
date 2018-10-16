41d97a98d31ca758a1ff9108fa623faaf6a22be3
David Schuppa <d.schuppa@gmail.com>
2018-10-16 14:59:16 +0200

    implemented LineItem class and it's methods

---
+++ src/main/java/com/codecool/shop/orderData/LineItem.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.orderData;
+
+import com.codecool.shop.model.Product;
+
+public class LineItem {
+    private String productName;
+    private int quantity = 0;
+    private double unitPrice;
+    private double subTotalPrice;
+    private Product product;
+
+
+    public LineItem(Product product) {
+        this.productName = product.getName();
+        this.unitPrice = product.getDefaultPrice();
+        this.subTotalPrice = unitPrice;
+        this.product = product;
+    }
+
+    public void setQuantity(){
+        this.quantity++;
+        subTotalPrice += unitPrice;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+        subTotalPrice = quantity * unitPrice;
+    }
+
+    public double getSubTotalPrice() {
+        return subTotalPrice;
+    }
+
+    public boolean compareProductId(int id) {
+        return product.getId() == id;
+    }
+}
