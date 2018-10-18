c3aba7a676b697f5f71f859cea19f5801a41837b
Uj Levente <asdlevente@gmail.com>
2018-10-18 18:59:13 +0200

    cart empty on payment feature implemented

---
+++ src/main/webapp/static/javascript/setCartEmpty.js
@@ -0,0 +1,15 @@
+function setEventListener() {
+    $("#payment-btn").click(emptyTheCartList)
+}
+
+function emptyTheCartList() {
+    $.post("/remove-from-cart", {setCartEmpty:"true"});
+}
+
+function init() {
+    setEventListener();
+}
+
+$( document ).ready(function() {
+    init();
+});
