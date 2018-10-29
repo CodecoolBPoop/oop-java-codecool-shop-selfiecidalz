6fce67c6987bb271e7169061c83cbfdf428236f6
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-29 12:08:42 +0100

    created sendemail class

---
+++ src/main/java/com/codecool/shop/orderData/SendEmail.java
@@ -0,0 +1,47 @@
+package com.codecool.shop.orderData;
+
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmail {
+
+    public static void sendEmail() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("selfiecidalz","s3lf13c1d4lz");
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from@no-spam.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(Order.getInstance().getCostumer().getEmail()));
+            message.setSubject("Testing Subject");
+            message.setText(Order.getInstance().toString());
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            System.out.println("Nemoké");
+        }
+    }
+}
+
