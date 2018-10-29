b822b6c844294aee71844b2f772103c4d79136cb
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-29 12:07:13 +0100

    send email method called

--- src/main/java/com/codecool/shop/ajaxHandler/Payment.java
+++ src/main/java/com/codecool/shop/ajaxHandler/Payment.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.ajaxHandler;
 
 import com.codecool.shop.orderData.Order;
+import com.codecool.shop.orderData.SendEmail;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +16,9 @@ public class Payment extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //empty the cart
+        SendEmail.sendEmail();
         Order.getInstance().emptyCartList();
+        System.out.println("ITT");
+        System.out.println(Order.getInstance().getCartList().size());
     }
 }
