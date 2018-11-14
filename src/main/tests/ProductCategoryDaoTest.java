89cae340cd466b6daa996de8c22d8f227cdc91d8
Uj Levente <asdlevente@gmail.com>
2018-11-14 19:35:20 +0100

    database and mem testing made easyily changable get all method tests implemented

--- src/main/tests/ProductCategoryDaoTest.java
+++ src/main/tests/ProductCategoryDaoTest.java
@@ -7,19 +7,28 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.codecool.shop.dao.DbConnection.getConnection;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryDaoTest {
 
-    private static ProductCategoryDao productCategoryData = ProductCategoryDaoJdbc.getInstance();
+    private static boolean isDbTesting = true;
+    private static ProductCategoryDao productCategoryData;
 
     @BeforeAll
     public static void setUp() {
 
-        DbConnection.setTestingDatabase();
+        if (isDbTesting) {
+            DbConnection.setTestingDatabase();
+            productCategoryData = ProductCategoryDaoJdbc.getInstance();
+        } else
+            productCategoryData = ProductCategoryDaoMem.getInstance();
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory(
@@ -38,26 +47,40 @@ class ProductCategoryDaoTest {
         productCategoryData.add(mobile);
     }
 
+    @AfterAll
+    static void emptyDatabase() {
 
+        if (isDbTesting) {
+            try (Connection connection = DbConnection.getConnection();
+                 PreparedStatement emptyTable = connection.prepareStatement("DELETE FROM categories;")) {
+
+                emptyTable.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+}
 
     @Test
-    void testGetAllMethod() {
+    void isGetAllMethodReturnsSameAmountOfObjects() {
 
         int numOfAddedCategories = 3;
-        //assertEquals(numOfAddedCategories, productCategoryData.getAll().size());
-
-        for (ProductCategory pc: productCategoryData.getAll()) {
-        }
+        assertEquals(numOfAddedCategories, productCategoryData.getAll().size());
     }
 
     @Test
-    public void testAddMethod() {
+    void isGetAllMethodReturnsTheSameObjectAdded() {
 
-
+        ProductCategory television = new ProductCategory(
+                4,"Television", "Hardware",
+                "These are televisions.");
+        productCategoryData.add(television);
+        //assertEquals(television, productCategoryData.getAll().stream().findFirst(x -> x == television));
     }
 
+
     @Test
-    public void testRemoveMethod() {
+    void testRemoveMethod() {
 
 
     }
