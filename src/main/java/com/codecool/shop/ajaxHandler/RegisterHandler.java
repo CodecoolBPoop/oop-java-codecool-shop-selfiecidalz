51966389073b789a3218af264d77b5d911f89068
adamszabolcs <szabolcs.adam91@gmail.com>
2018-11-28 11:25:04 +0100

    created class, started to implement information sending

---
+++ src/main/java/com/codecool/shop/ajaxHandler/RegisterHandler.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.ajaxHandler;
+
+import com.codecool.shop.bcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/register-handler"})
+public class RegisterHandler extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = BCrypt.hashpw(req.getParameter("password"), BCrypt.gensalt());
+        String email = req.getParameter("email");
+        String billingAddress = req.getParameter("billing");
+        String shippingAddress = req.getParameter("shipping");
+
+    }
